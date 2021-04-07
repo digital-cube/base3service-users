6743c236a61bea8e6e19927708448bb04a4a1fb3
Slobodan Dolinic <slobodan@digitalcube.rs>
2021-04-07 13:21:43 +0200

    wait for db

--- service.sh
+++ service.sh
@@ -1,5 +1,8 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-cd /app && python wait4db.py && aerich upgrade &&  python service.py
-
-#cd /app && python wait4db.py && aerich upgrade && sleep 1 && python scripts/tmp/seed_users.py && python service.py
+if [[ $APP_ENVIRONMENT == "prod" ]]
+then
+  cd /app && aerich upgrade &&  python service.py
+else
+  cd /app && python wait4db.py && aerich upgrade &&  python service.py
+fi
